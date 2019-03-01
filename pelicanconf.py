e46c35f26dbb47359f07e5047b83bdcb68ddcc9f
DZ84 <gzuz13@hotmail.com>
2019-03-01 02:33:26 +0100

    some small adjustments

--- pelicanconf.py
+++ pelicanconf.py
@@ -79,6 +79,8 @@ MENUITEMS = (
     ('About', '/pages/About.html'),
 )
 
+DISQUS_SITENAME = 'site-ceilings'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
